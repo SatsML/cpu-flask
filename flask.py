c5feedf6400a86fdbff284103c1f850257407855
SatsML <mailtosatyamsaini1@gmail.com>
2025-05-08 20:28:08 +0530

    Update flask.py

--- flask.py
+++ flask.py
@@ -1,12 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
 import psutil
-
 app = Flask(__name__)
-
 @app.route("/")
 def index():
-    cpu = psutil.cpu_percent(interval=1)
-    return render_template("index.html", cpu=cpu)
-
+ cpu = psutil.cpu_percent(interval=1)
+ return render_template("index.html",cpu=cpu)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+ app.run(host="0.0.0.0",port=5000)
